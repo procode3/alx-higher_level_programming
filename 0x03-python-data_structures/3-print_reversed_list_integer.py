73fac4f0cb18c931307ec9bd96eeaab86a62de8d
Erick <wr.t8r1@gmail.com>
2022-12-04 10:11:36 -0800

    Used string compression

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    n = len(my_list) - 1
-    for i in my_list:
-        print("{}".format(my_list[n]))
-        n = n - 1
+    [print("{}".format(my_list[i])) for i in my_list]
