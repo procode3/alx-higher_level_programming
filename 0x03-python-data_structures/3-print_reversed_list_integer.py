284584e0587852ea738a1f473ecfe08328a228e4
Erick <wr.t8r1@gmail.com>
2022-12-04 10:23:00 -0800

    Fixed logic

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    [print("{}".format(my_list[i])) for i in my_list]
+    my_list.reverse()
+    for i in my_list:
+        print("{}".format(i))
+
+
+
+my_list = [1, 2, 3, 4, 5]
+
+print_reversed_list_integer(my_list)
+
+
