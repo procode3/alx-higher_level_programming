5bf27b1e5a9595129fd40ddc7e37939d6f542ef2
Erick <wr.t8r1@gmail.com>
2022-12-07 12:51:02 -0800

    solved 9 and 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        v = list(a_dictionary.values())
+        k = list(a_dictionary.keys())
+        return k[v.index(max(v))]
+    else:
+        return None
