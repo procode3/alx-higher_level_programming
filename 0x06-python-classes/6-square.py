1551b0dce4d93f5ed976109e57db08bd71d7aab8
Erick <wr.t8r1@gmail.com>
2022-12-23 02:44:57 -0800

    Fixed parsing 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,8 +13,8 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        if (type(position[0]) and type(position[1])) is not int or \
-                len(position) != 2 or position[0] < 0 or position[1] < 0:
+        if len(position) != 2 or (type(position[0]) and type(position[1])) \
+                is not int or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
@@ -41,8 +41,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value[0]) and type(value[1]) is not int or \
-                len(value) != 2 or value[0] < 0 or value[1] < 0:
+        if len(position) != 2 or type(value[0]) and type(value[1]) is not int \
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
