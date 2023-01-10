85fe636adbe36aa28aaa678a7f7b9f0a6755d681
procode3 <wr.t8r1@gmail.com>
2023-01-11 00:01:50 +0300

    Python to Json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,12 @@ import json
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
-my_list = load("add_item.json")
-my_list = my_list + sys.argv[1:]
-save(my_list, "add_item.json")
+args = sys.argv[1:]
+jsonfile = "add_item.json"
+try:
+    my_list = load(jsonfile)
+except FileNotFoundError:
+    save(args, jsonfile)
+else:
+    my_list.extend(args)
+    save(my_list, jsonfile)
