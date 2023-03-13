71ce6cbd78295887234e24c5b65a741f6fd8202a
procode3 <wr.t8r1@gmail.com>
2023-03-14 00:16:32 +0300

    Loop and factorial

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   const array = process.argv.slice(2, process.argv.length);
-  array.sort(function (a, b) { return a - b; });
-  array.reverse();
+  array.sort(function (a, b) { return b - a; });
   console.log(array[1]);
 }
