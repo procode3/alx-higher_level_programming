1bd35a69b7f2ba0c00443d4a334642e713df8de8
procode3 <wr.t8r1@gmail.com>
2023-03-13 22:23:43 +0300

    Loop and Argvwq

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-if (isNaN(Number(process.argv[2]))) {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-for (let i = 0; i < Math.trunc(process.argv[2]); i++)
-  console.log('C is fun');
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
 }
