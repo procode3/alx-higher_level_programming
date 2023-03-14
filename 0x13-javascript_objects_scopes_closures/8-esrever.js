629eefea3fba7f31ee2ecc15b6ab6e7718db9898
procode3 <wr.t8r1@gmail.com>
2023-03-14 22:31:59 +0300

    Class Method

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-let c = [];
+const c = [];
 exports.esrever = function (list) {
   for (let i = list.length - 1; i >= 0; i--) {
-    c.push(list[i])
+    c.push(list[i]);
   }
   return c;
 };
