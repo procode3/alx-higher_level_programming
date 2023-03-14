292d900d63f615e6e5420293ec0b1f1103ed61a0
procode3 <wr.t8r1@gmail.com>
2023-03-14 21:56:42 +0300

    Class Method

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      count++;
+    }
+  }
+  return count;
+}
+
