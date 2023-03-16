9d18c734a56d2d4ffe1deb314b7e436423800edc
procode3 <wr.t8r1@gmail.com>
2023-03-16 13:59:51 +0300

    MySDLdb Connector

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" selecting with mysqlDb """
+import MySQLdb
+import sys
+
+if __name__ = "__main__":
+	try:
+		db_conn = MySQLdb.connect
+		(host = "localhost", user = sys.arg[1], passwd = sys.arg[2], db = sys.argv[3])
+
+	except:
+		print("Failed to connect to db")
+		return 0 
+
+	cur = db_conn.cursor()
+	cur.exercute ("SELECET * FROM state ORDER BT states.id")
+	cur.fetchall()
+	for row in rows:
+		print(row);
+
