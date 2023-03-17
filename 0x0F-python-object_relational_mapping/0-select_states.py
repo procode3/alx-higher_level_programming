78d1701ba81f96adc3a829f89b6a8c51f0ef3528
procode3 <wr.t8r1@gmail.com>
2023-03-17 10:37:03 +0300

    Fixing shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,10 @@ import MySQLdb
 import sys
 
 if __name__ = "__main__":
-	try:
-		db_conn = MySQLdb.connect
-		(host = "localhost", user = sys.arg[1], passwd = sys.arg[2], db = sys.argv[3])
+	db_conn = MySQLdb.connect
+	(host = "localhost", user = sys.arg[1], passwd = sys.arg[2], db = sys.argv[3])
 
-	except:
-		print("Failed to connect to db")
-		return 0 
+	print("Failed to connect to db")
 
 	cur = db_conn.cursor()
 	cur.exercute ("SELECET * FROM state ORDER BT states.id")
