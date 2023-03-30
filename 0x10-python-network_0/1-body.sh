8798141b7b734438d02e270a2091888bb48c3317
procode3 <wr.t8r1@gmail.com>
2023-03-30 21:14:11 +0300

    Find size of response body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response only for status 200
+# send a request to an URL with curl, 
 
 curl -s -o /dev/null -w "%{http_code}" $1 | grep -q 200 && curl -s $1
 
