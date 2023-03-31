bbd7ea7123277a47869533990f8ea4c3e3897b1f
procode3 <wr.t8r1@gmail.com>
2023-03-31 15:27:30 +0300

    Printing out the error codes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         res = requests.get(av[1])
         error = res.raise_for_status()
         if not error:
-            print(res.text)	
+            print(res.text)
     except requests.exceptions.HTTPError as error:
         if (res.status_code > 400):
             print(f'Error code: {res.status_code}')
