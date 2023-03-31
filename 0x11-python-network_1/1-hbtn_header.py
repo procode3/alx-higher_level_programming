e289ac9a2e105b739fc7b26a24542a848b0f5c07
procode3 <wr.t8r1@gmail.com>
2023-03-31 13:08:40 +0300

    Get req Header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 """A Python script that fetches
 https://alx-intranet.hbtn.io/status
 """
-import sys
-from urllib.request import urlopen
 
-with urlopen(sys.argv[1]) as f:
-     print(f.getheader('X-Request-Id'))
+if __name__ == '__main__'
+    import sys
+    from urllib.request import urlopen
+    with urlopen(sys.argv[1]) as f:
+        print(f.getheader('X-Request-Id'))
