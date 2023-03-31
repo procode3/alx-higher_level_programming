39d8d5a38e0f5c86786f47d9c10dfb1ccb47df7e
procode3 <wr.t8r1@gmail.com>
2023-03-31 13:07:12 +0300

    Post req

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,18 +3,11 @@
 https://alx-intranet.hbtn.io/status
 """
 
-if __name__ != '__main__':
-	exit()
-import sys
-from urllib import request, parse
-
-kv = {'email': sys.argv[2]}
-data = parse.urlencode(kv).encode()
-
-req = request.Request(sys.argv[1], data=data)
-
-with request.urlopen(req) as res:
-	print(res.read().decode("utf-8"))
-
-
-	
+if __name__ == '__main__':
+    import sys
+    from urllib import request, parse
+    kv = {'email': sys.argv[2]}
+    data = parse.urlencode(kv).encode()
+    req = request.Request(sys.argv[1], data=data)
+    with request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
