aa9ab1bbc92aa40de226229958ee084c3bd184d3
procode3 <wr.t8r1@gmail.com>
2023-03-31 13:19:15 +0300

    Fetch a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
         print('Body response:')
         print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
-        print(f'\t- utf8 contnent: {html.decode("utf-8")}')
+        print(f'\t- utf8 content: {html.decode("utf-8")}')
