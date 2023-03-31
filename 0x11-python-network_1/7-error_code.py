faa4c32f67b9cc28b4d485e0732698fae7493a68
procode3 <wr.t8r1@gmail.com>
2023-03-31 15:26:52 +0300

    Printing out the error codes

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Get a request using requests package"""
+
+import requests
+from sys import argv as av
+
+if __name__ == '__main__':
+    try:
+        res = requests.get(av[1])
+        error = res.raise_for_status()
+        if not error:
+            print(res.text)	
+    except requests.exceptions.HTTPError as error:
+        if (res.status_code > 400):
+            print(f'Error code: {res.status_code}')
