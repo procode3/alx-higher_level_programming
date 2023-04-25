70a9f0c8729a9874ef4fcec755f0f0e864072519
procode3 <wr.t8r1@gmail.com>
2023-04-25 14:29:51 +0300

    Reading file with JS

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const info = process.argv[3] + '\n';
+const info = process.argv[3];
 
 fs.writeFile(filePath, info, (err) => {
   if (err) {
